dc8f6a7c9ee83b62ce2b673a31945544b799411b
Jos Castro <joselycas@gmail.com>
2018-08-13 14:06:38 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,6 @@ class Pokemon
 
   def self.find(id, db)
     db.execute 'SELECT * FROM pokemon WHERE id = ?', id do |row|
-      binding.pry
       puts row
     end
     # self.new(id = id, name = name, type = type, db = db)
